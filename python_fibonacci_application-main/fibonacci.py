54b792e784f9a93c2ea8c1ee19dc3e1ca0dec11b
nathantotten <nathan.james.totten@gmail.com>
2023-10-25 20:27:25 +0100

    Minor changes

--- python_fibonacci_application-main/fibonacci.py
+++ python_fibonacci_application-main/fibonacci.py
@@ -21,7 +21,7 @@ def fibonacci(n):
 
 # Create a command-line argument parser
 parser = argparse.ArgumentParser(description='Compute Fibonacci sequence of a specified length.')
-parser.add_argument('--limit', type=int, default=100000, help='Length of the Fibonacci sequence')
+parser.add_argument('--limit', type=int, default=200000, help='Length of the Fibonacci sequence')
 
 # Parse the command-line arguments
 args = parser.parse_args()
@@ -34,9 +34,9 @@ limit = args.limit
 
 # Calculate the Fibonacci sequence
 #with cProfile.Profile() as Profile:
- #   result = fibonacci(limit)
+result = fibonacci(limit)
 
-fibonacci(limit)
+
 
 #End execution timer
 #end = timer()
