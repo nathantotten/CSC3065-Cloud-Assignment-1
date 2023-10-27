a90bb19d4518217a2f774d3aabb3f86566a5e42f
nathantotten <nathan.james.totten@gmail.com>
2023-10-27 22:39:14 +0100

    Some alterations

--- python_fibonacci_application-main/fibonacci.py
+++ python_fibonacci_application-main/fibonacci.py
@@ -1,8 +1,9 @@
 import argparse
-from resource import *
+import sys
 from time import sleep
 from timeit import default_timer as timer
-import cProfile
+
+sys.set_int_max_str_digits(50000)
 
 def fibonacci(n):
     if n <= 0:
@@ -21,7 +22,7 @@ def fibonacci(n):
 
 # Create a command-line argument parser
 parser = argparse.ArgumentParser(description='Compute Fibonacci sequence of a specified length.')
-parser.add_argument('--limit', type=int, default=200000, help='Length of the Fibonacci sequence')
+parser.add_argument('--limit', type=int, default=2000000, help='Length of the Fibonacci sequence')
 
 # Parse the command-line arguments
 args = parser.parse_args()
@@ -29,22 +30,20 @@ args = parser.parse_args()
 # Get the limit from the command-line arguments
 limit = args.limit
 
-#Start execution timer
 #start = timer()
 
 # Calculate the Fibonacci sequence
-#with cProfile.Profile() as Profile:
 result = fibonacci(limit)
 
 
 
-#End execution timer
+
 #end = timer()
 
-print(f"Computed Fibonacci sequence of length {len(result)}: {result}")
+#print(f"Computed Fibonacci sequence of length {len(result)}: {result}")
 #print(end - start)
-#Profile.print_stats()
-#print(getrusage(RUSAGE_SELF))
+
+
 
 # This is helpful https://www.geeksforgeeks.org/how-to-get-current-cpu-and-ram-usage-in-python/
 # And this! https://www-h.eng.cam.ac.uk/help/languages/python/pythonmemory.html
